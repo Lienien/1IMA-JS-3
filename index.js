e3ae22ffcf1c28f4413357e08c2e943c5f53dc00
Lienien <116272320+Lienien@users.noreply.github.com>
2023-02-16 12:32:24 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+//Oppgave 1
+let i = 0;
+while (i <= 50) {
+    console.log(i)
+    i++
+}
+
+//Oppgave 2
+let o = 2;
+while (o <= 100) {
+    console.log(o)
+    o=o+2
+}
+
+//Oppgave 3
+let p = 0
+while (p <= 999) {
+    console.log(p)
+    p=p+3
+}
+
+//Oppgave 4
+let j = 15;
+while (j <= 1000) {
+    console.log(j)
+    j=j+15
+}
+
+//oppgave 5
+let k = 0
+let sum = 0
+while (k <= 100) {
+    sum = sum + k
+    k++
+}
+console.log(sum)
+
+//Oppgave 6
+
+let l = 0
+let text = " "
+while (l <= 4) {
+    text += "#"
+    console.log(text)
+    l++
+}
+
+//Oppgave 7
+
+let b = 0
+let text2 = " "
+while (b <= 50) {
+    text2 += "#"
+    console.log(text2)
+    b++
+}
+
+//Oppgave 8
